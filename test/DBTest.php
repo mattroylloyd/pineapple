2ee5e1785409501498c3917a9100cbc79222b256
mattroylloyd <matthew.lloyd@mayden.co.uk>
2016-10-13 01:53:00 +0100

    Rewrite DB tests with single assertions

--- test/DBTest.php
+++ test/DBTest.php
@@ -9,112 +9,148 @@ use PHPUnit\Framework\TestCase;
 
 class DBTest extends TestCase
 {
-    public function testFactory()
+    public function testFactoryCreatesCorrectDriver()
     {
-        $dbh = DB::factory(TestDriver::class);
-        $this->assertInstanceOf(TestDriver::class, $dbh);
-        $this->assertFalse($dbh->getOption('persistent'));
+        $driver = DB::factory(TestDriver::class);
+
+        $this->assertInstanceOf(TestDriver::class, $driver);
+    }
+
+    public function testDefaultPersistentOptionIsFalse()
+    {
+        $driver = DB::factory(TestDriver::class);
+
+        $this->assertFalse($driver->getOption('persistent'));
+    }
+
+    public function testFactoryCreatesCorrectDriverWithArrayOptions()
+    {
+        $driver = DB::factory(TestDriver::class, ['debug' => 'q who?']);
+
+        $this->assertInstanceOf(TestDriver::class, $driver);
     }
 
     public function testFactoryWithArrayOptions()
     {
-        $dbh = DB::factory(TestDriver::class, ['debug' => 'q who?']);
-        $this->assertInstanceOf(TestDriver::class, $dbh);
-        $this->assertEquals('q who?', $dbh->getOption('debug'));
+        $driver = DB::factory(TestDriver::class, ['debug' => 'q who?']);
+
+        $expected = 'q who?';
+        $actual = $driver->getOption('debug');
+
+        $this->assertEquals($expected, $actual);
     }
 
-    public function testFactoryWithLegacyOption()
+    public function testFactoryCreatesCorrectDriverWithLegacyOption()
     {
-        $dbh = DB::factory(TestDriver::class, true);
-        $this->assertInstanceOf(TestDriver::class, $dbh);
-        $this->assertTrue($dbh->getOption('persistent'));
+        $driver = DB::factory(TestDriver::class, true);
+
+        $this->assertInstanceOf(TestDriver::class, $driver);
     }
 
-    public function testFactoryWithBadDriver()
+    public function testDefaultPersistentWithLegacyOptionIsTrue()
     {
-        $dbh = DB::factory('murp');
-        $this->assertInstanceOf(Error::class, $dbh);
+        $driver = DB::factory(TestDriver::class, true);
+
+        $this->assertTrue($driver->getOption('persistent'));
+    }
+
+    public function testFactoryWithBadDriverReturnsError()
+    {
+        $badDriver = DB::factory('murp');
+
+        $this->assertInstanceOf(Error::class, $badDriver);
     }
 
     public function testIsConnection()
     {
-        $dbh = DB::factory(TestDriver::class);
-        $this->assertTrue(DB::isConnection($dbh));
+        $driver = DB::factory(TestDriver::class);
+
+        $this->assertTrue(DB::isConnection($driver));
     }
 
     public function testIsNotConnection()
     {
-        $this->assertFalse(DB::isConnection(new \stdClass));
+        $badDriver = new \stdClass;
+
+        $this->assertFalse(DB::isConnection($badDriver));
     }
 
-    public function testIsManip()
+    /**
+     * @dataProvider manipulationStatementsDataProvider
+     */
+    public function testManipulationStatementsAreCorrectlyIdentified($statement)
     {
-        $this->assertTrue(DB::isManip('
-            INSERT INTO things (foo, bar) VALUES (1, 2)
-        '));
-        $this->assertTrue(DB::isManip('
-            UPDATE things
-               SET bar = 2
-             WHERE foo = 1
-        '));
-        $this->assertTrue(DB::isManip('
-            DELETE FROM things
-                  WHERE foo = 1
-        '));
-        $this->assertTrue(DB::isManip('
-            REPLACE INTO things (foo, bar) VALUES (1, 2)
-        '));
-        $this->assertTrue(DB::isManip('
-            CREATE TABLE things (id INT PRIMARY KEY NOT NULL AUTO_INCREMENT)
-        '));
-        $this->assertTrue(DB::isManip('
-            DROP TABLE things
-        '));
-        $this->assertTrue(DB::isManip('
-            LOAD DATA INFILE "foo.dat"
-                        INTO things
-        '));
-        $this->assertTrue(DB::isManip('
-            SELECT foo, bar, baz
-              INTO stuff
-              FROM things
-        '));
-        $this->assertTrue(DB::isManip('
-            ALTER TABLE whatnot
-            DROP COLUMN id
-        '));
-        $this->assertTrue(DB::isManip('
-            GRANT SELECT ON things TO nobody@"%"
-        '));
-        $this->assertTrue(DB::isManip('
-            REVOKE SELECT ON things FROM nobody@"%"
-        '));
-        $this->assertTrue(DB::isManip('
-            LOCK TABLE things
-        '));
-        $this->assertTrue(DB::isManip('
-            UNLOCK TABLE things
-        '));
+        $this->assertTrue(DB::isManip($statement));
     }
 
-    public function testIsNotManip()
+    public function testNonManipulationStatementsAreCorrectlyIdentified()
     {
-        $this->assertFalse(DB::isManip('
+        $statement = '
             SELECT lyrics
               FROM track
              WHERE title = "useless"
                AND artist = "depeche mode"
-        '));
+        ';
+
+        $this->assertFalse(DB::isManip($statement));
     }
 
-    public function testErrorMessage()
+    public function testErrorMessageForDbOkStatusIsNoError()
     {
-        $this->assertEquals('no error', DB::errorMessage(DB::DB_OK));
+        $message = DB::errorMessage(DB::DB_OK);
+
+        $this->assertEquals('no error', $message);
     }
 
     public function testErrorMessageFromErrorClass()
     {
-        $error = new DBError(DB::DB_ERROR_NOT_FOUND);
-        $this->assertEquals('not found', DB::errorMessage($error));
+        $message = new DBError(DB::DB_ERROR_NOT_FOUND);
+
+        $this->assertEquals('not found', DB::errorMessage($message));
+    }
+
+    protected function manipulationStatements()
+    {
+        return [
+            'INSERT INTO things (foo, bar) VALUES (1, 2)',
+
+            'UPDATE things
+               SET bar = 2
+             WHERE foo = 1',
+
+            'DELETE FROM things
+                  WHERE foo = 1',
+
+            'REPLACE INTO things (foo, bar) VALUES (1, 2)',
+
+            'CREATE TABLE things (id INT PRIMARY KEY NOT NULL AUTO_INCREMENT)',
+
+            'DROP TABLE things',
+
+            'LOAD DATA INFILE "foo.dat"
+                        INTO things',
+
+            'SELECT foo, bar, baz
+              INTO stuff
+              FROM things',
+
+            'ALTER TABLE whatnot
+            DROP COLUMN id',
+
+            'GRANT SELECT ON things TO nobody@"%"',
+
+            'REVOKE SELECT ON things FROM nobody@"%"',
+
+            'LOCK TABLE things',
+
+            'UNLOCK TABLE things',
+        ];
+    }
+
+    public function manipulationStatementsDataProvider()
+    {
+        return array_map(function ($x) {
+          return (array) $x;
+        }, $this->manipulationStatements());
     }
 }
